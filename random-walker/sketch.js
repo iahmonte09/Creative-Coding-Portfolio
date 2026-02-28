77b51b128937c8789b6fde6e6c5280fb76d35f73
Bithiah Monte <iahmonte09@gmail.com>
2026-02-28 18:07:36 +0300

    Implemented random walker functionality in sketch.js
    added code for random walkeer exercise

--- random-walker/sketch.js
+++ random-walker/sketch.js
@@ -1,1 +1,26 @@
+let x;
+let y;
 
+function setup() {
+  createCanvas(600, 400);
+  background(255);
+  x = width / 2;
+  y = height / 2;
+}
+
+function draw() {
+  stroke(0);
+  point(x, y);
+
+  let step = floor(random(4));
+
+  if (step === 0) {
+    x += 5;
+  } else if (step === 1) {
+    x -= 5;
+  } else if (step === 2) {
+    y += 5;
+  } else {
+    y -= 5;
+  }
+}
